c92ad4767ae0483145f96b0695278cb05d5ff720
kim chae yeon <chae9750@gmail.com>
2021-06-29 13:41:27 +0900

    gitignore 수정

--- admin/admin_site/models.py
+++ admin/admin_site/models.py
@@ -25,8 +25,13 @@ GOODS_FEATURE = (
     ('기타', '기타'),
 )
 
+AUTHORITY = (
+    ('master', 'master'),
+    ('user', 'user'),
+)
+
 class User(models.Model):
-    authority = models.CharField(max_length=10, choices=(('master', 'master'), ('user', 'user'),))
+    authority = models.CharField(max_length=10, choices=AUTHORITY)
     user_id = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
