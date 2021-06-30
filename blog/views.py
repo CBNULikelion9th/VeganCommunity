acfe56a44124676155fb999896b73e87c56aacc7
Limhyunjung9 <guswjd951622@naver.com>
2021-07-01 01:55:32 +0900

    views.py 변수이름 수정

--- blog/views.py
+++ blog/views.py
@@ -5,31 +5,29 @@ from .models import Post
 
 # Create your views here.
 
-def post_map(request):
-    return render(request, 'blog/map.html')
 
-def post_main(request):
+def map_main(request):
     return render(request, 'blog/main.html')
 
-def post_info(request):
+def vegan_info(request):
     return render(request, 'blog/info.html')
 
-def post_save(request):
+def store_save(request):
     return render(request, 'blog/save.html')
 
-def post_report(request):
+def area_report(request):
     if request.method == 'GET':
         form = ReportForm()
 
-    return render(request, 'blog/report.html', {
-        'form' : form,
+        return render(request, 'blog/report.html', {
+           'form' : form,
     })
 
-def post_add(request):
+def vegan_area_add(request):
     if request.method == 'GET':
         form = PostForm()
 
-    return render(request, 'blog/add.html', {
-        'form' : form,
+        return render(request, 'blog/add.html', {
+            'form' : form,
     })
 
