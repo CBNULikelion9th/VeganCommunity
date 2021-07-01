7def5a3d8441b4c41560be126ea4f8daf6187e48
unknown <yukmj1123@naver.com>
2021-07-01 15:24:22 +0900

    signup

--- loginproject/loginapp/forms.py
+++ loginproject/loginapp/forms.py
@@ -2,16 +2,4 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-#회원 가입 폼
-class UserForm(UserCreationForm):
-    email = forms.EmailField(label="이메일")
 
-    class Meta:
-        model = User
-        fields = ("username", "email")
-
-#회원 가입 폼2
-class signform(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField()
-    email = forms.CharField()
