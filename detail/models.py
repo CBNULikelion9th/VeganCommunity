35c5e028f23a0d109beaa093d2a837f24e32ed13
JuYeong <happyu9986@gmail.com>
2021-07-15 11:53:04 +0900

    show filtering by category

--- detail/models.py
+++ detail/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 from django.conf import settings
 
-class Vegan(models.Model):
-    f1 = models.TextField(default="밀가루")
-    f2 = models.TextField(default="돼지고기")
-    f3 = models.TextField(default="닭가슴살")
-    f4 = models.TextField(default="채소")
-
 class Ingredient(models.Model):
-    f1 = models.TextField(default="샐러드")
-    f2 = models.TextField(default="건강식")
-    f3 = models.TextField(default="계란")
-    f4 = models.TextField(default="바지락")
+    f1 = models.TextField(verbose_name='나물')
+    f2 = models.TextField(verbose_name='생선')
+    f3 = models.TextField(verbose_name='떡')
+    f4 = models.TextField(verbose_name='국')
+    f5 = models.TextField(verbose_name='면')
 
-class Product(models.Model):
-    f1 = models.TextField(default="샐러드")
-    f2 = models.TextField(default="프로틴")
-    f3 = models.TextField(default="찌개")
-    f4 = models.TextField(default="저칼로리")
+class FoodNutrients(models.Model):
+    f1 = models.TextField(verbose_name='열량')
+    f2 = models.TextField(verbose_name='탄수화물')
+    f3 = models.TextField(verbose_name='단백질')
+    f4 = models.TextField(verbose_name='지방')
+    f5 = models.TextField(verbose_name='당류')
+    f6 = models.TextField(verbose_name='콜레스테롤')
 
-class Shopping(models.Model):
-    f1 = models.TextField(default="단백질")
-    f2 = models.TextField(default="탄수화물")
-    f3 = models.TextField(default="반찬가게")
-    f4 = models.TextField(default="다이어트 식단")
+class Products(models.Model):
+    f1 = models.TextField(verbose_name='채소류')
+    f2 = models.TextField(verbose_name='과일류')
+    f3 = models.TextField(verbose_name='곡류')
+
+class ShoppingMall(models.Model):
+    f1 = models.TextField(verbose_name='마켓컬리')
+    f2 = models.TextField(verbose_name='쿠팡')
+    f3 = models.TextField(verbose_name='푸드슈퍼마켓')
+    f4 = models.TextField(verbose_name='G마켓')
--- detail/views.py
+++ detail/views.py
@@ -11,7 +11,7 @@ import urllib.request as ul
 import xmltodict
 from . import food_info
 from . import foodapi
-from .models import Vegan, Ingredient, Product, Shopping
+from .models import Ingredient, FoodNutrients, Products, ShoppingMall
 
 def info(request):
     # result = food_info.info()
@@ -19,9 +19,16 @@ def info(request):
     #     'result_1' : result["response"]["body"]["items"]["item"][1]["RPRSNT_RAWMTRL_NM"],
     #     'result_2' : result["response"]["body"]["items"]["item"][0]["RPRSNT_RAWMTRL_NM"]
     # }
-
+    category = Ingredient(f1="나물",f2="생선",f3="떡",f4="국",f5="면")
+    # category2 = FoodNutrients.objects.get()
+    # category3 = Products.objects.get()
+    # category4 = ShoppingMall.objects.get()
     result = foodapi.read_data()
     context ={
+        'category':category,
+        # 'category':category2,
+        # 'category':category3,
+        # 'category':category4,
         'index' : result["I2790"]["row"][3]["NUM"],
         'result_1' : result["I2790"]["row"][4]["DESC_KOR"]
     }
@@ -29,7 +36,9 @@ def info(request):
     return render(request,'detail/main.html',context)
 
 def category1(request):
-    category = Vegan.objects.get(pk=1)
+    # category = Ingredient.objects.get()
+    category = Ingredient(f1="나물",f2="생선",f3="떡",f4="국",f5="면")
+    # category.save()
     check=1
     if request.method == "POST":
         context = {
@@ -39,7 +48,8 @@ def category1(request):
     return render(request,'detail/main.html',context)
 
 def category2(request):
-    category = Ingredient.objects.get(pk=1)
+    # category = FoodNutrients.objects.get()
+    category =  FoodNutrients(f1="열량",f2="탄수화물",f3="단백질",f4="지방",f5="당류",f6="콜레스테롤")
     check=2
     if request.method == "POST":           
         context = {
@@ -49,7 +59,8 @@ def category2(request):
     return render(request,'detail/main.html',context)
 
 def category3(request):
-    category = Product.objects.get(pk=1)
+    # category = Products.objects.get()
+    category = Products(f1="채소류",f2="과일류",f3="곡류")
     check=3
     if request.method == "POST":
         context = {
@@ -59,7 +70,8 @@ def category3(request):
     return render(request,'detail/main.html',context)
 
 def category4(request):
-    category = Shopping.objects.get(pk=1)
+    # category = ShoppingMall.objects.get()
+    category = ShoppingMall(f1="마켓컬리",f2="쿠팡",f3="푸드슈퍼마켓",f4="G마켓")
     check=4
     if request.method == "POST": 
         context = {
