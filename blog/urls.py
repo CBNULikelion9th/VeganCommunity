35f9de0c40fe9e9bb45e5cc22501be80f36af896
Limhyunjung9 <guswjd951622@naver.com>
2021-08-04 22:32:55 +0900

    상세페이지->팝업변경을 위한 페이지 삭제

--- blog/urls.py
+++ blog/urls.py
@@ -13,17 +13,6 @@ urlpatterns = [
 
 
 
-    path('store_1/', views.store_1, name='store_1'),
-    path('store_2/', views.store_2, name='store_2'),
-    path('store_3/', views.store_3, name='store_3'),
-    path('store_4/', views.store_4, name='store_4'),
-    path('store_5/', views.store_5, name='store_5'),
-    path('store_6/', views.store_6, name='store_6'),
-    path('store_7/', views.store_7, name='store_7'),
-    path('store_8/', views.store_8, name='store_8'),
-    path('store_9/', views.store_9, name='store_9'),
-    path('store_10/', views.store_10, name='store_10'),
-
     
 
 ]
