f05663f8a9a223645c4906d47e8a6f70763a1403
JuYeong <happyu9986@gmail.com>
2021-08-08 18:19:43 +0900

    img 크롤링 X

--- detail/jsondata/cate4_data.py
+++ detail/jsondata/cate4_data.py
@@ -1,8 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+# import urllib.request
 
 titles = []
 prices = []
+# imgs = []
 
 url_list = [
     "https://search.shopping.naver.com/search/all?query=%EB%A7%88%EC%BC%93%EC%BB%AC%EB%A6%AC&frm=NVSHATC&prevQuery=%EC%8B%9D%EB%A3%8C%ED%92%88",    # 마켓컬리
@@ -33,6 +35,17 @@ def getNameValue(url, tag, className) :
     # print(price_list)
     return price_list
 
+# def getImgValue(url, tag, className):
+#     img_list = []
+#     html = urlopen(url)
+#     bs = BeautifulSoup(html, "html.parser")
+#     content = bs.find(tag, class_= className)
+#     # print(content.get_text())
+#     # for i in content:
+#     img_url = content.find("img")["src"]
+#     # print(img_list)
+#     return img_url
+
 def getshop():
 
     tag = 'a'
@@ -48,11 +61,28 @@ def getshop():
         prices.append(getNameValue(url, tag, className))
     print(prices)
 
-    # context = {
-    #     'titles':titles,
-    #     'prices':prices
-    # }
+    # tag =  'a'
+    # className = 'thumbnail_thumb__3Agq6'
+    # for url in url_list :
+    #     imgs.append(getImgValue(url, tag, className))
+    # print(imgs)
 
-    # return context
 
-getshop()
+# def getimg():
+#     url = "https://search.shopping.naver.com/search/all?query=%EC%BF%A0%ED%8C%A1%20%EB%B0%98%EC%B0%AC&frm=NVSHATC&prevQuery=%EC%BF%A0%ED%8C%A1"
+#     fp = urllib.request.urlopen(url)
+#     source = fp.read();
+#     fp.close()
+
+#     #소스에서 img_area 클래스 하위의 소스를 가져온다.
+#     soup = BeautifulSoup(source, 'html.parser')
+#     soup = soup.find("div",class_ = "thumbnail_thumb_wrap__1pEkS _wrapper")
+#     print(soup)
+#     #이미지 경로를 받아온다.
+#     imgURL = soup.find("img")["src"]
+
+#     print(imgURL)
+
+
+# getimg()
+# getshop()
