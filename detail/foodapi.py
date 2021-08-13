72524bbdcfccb7e629397b3129ec842cd5fb7dd8
JuYeong <happyu9986@gmail.com>
2021-08-13 14:37:40 +0900

    decode에러 수정

--- detail/foodapi.py
+++ detail/foodapi.py
@@ -9,9 +9,9 @@ def read_data():
     GOOGLE_SEARCH_ENGINE_KEY = os.getenv('GOOGLE_SEARCH_ENGINE_KEY2')
     startRow = 1
     endRow = 150
-    url = 'http://openapi.foodsafetykorea.go.kr/api/'+GOOGLE_SEARCH_ENGINE_KEY+'/I2790/json/'+str(startRow)+'/'+str(endRow)
-
-    data = urllib.request.urlopen(url).read()
+    url = 'http://openapi.foodsafetykorea.go.kr/api/'+str(GOOGLE_SEARCH_ENGINE_KEY)+'/I2790/json/'+str(startRow)+'/'+str(endRow)
+    
+    data = urllib.request.urlopen(url).read().decode('utf-8')
     output = json.loads(data)
 
     return output
